73beacbf90cdd7b09c88a41371e27ebd0e2a80d4
hiropi1224 <hiropi1224s@gmail.com>
2024-03-12 23:19:49 +0900

    🔧 add biome config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@mantine/core/styles.css";
 
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="ja">
