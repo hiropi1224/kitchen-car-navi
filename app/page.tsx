41d1872b0502cc962c34a7929cb59d84c64411d5
hiropi1224 <hiropi1224s@gmail.com>
2024-03-12 22:53:31 +0900

    🔧 mantine config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import { Text } from "@mantine/core";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello, world
+      <Text
+        size="xl"
+        fw={900}
+        variant="gradient"
+        gradient={{ from: "blue", to: "cyan", deg: 90 }}
+      >
+        Hello, World
+      </Text>
     </main>
   );
 }
