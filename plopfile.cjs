9fd38a9fdcbce75aa601854f3a9afe3e49b961b4
hiropi1224 <hiropi1224s@gmail.com>
2024-03-13 21:48:14 +0900

    🔧 add plop config

---
+++ plopfile.cjs
@@ -0,0 +1,84 @@
+module.exports = (
+  /** @type {import('plop').NodePlopAPI} */
+  plop,
+) => {
+  plop.setGenerator("component", {
+    description: "create a new component",
+    prompts: [
+      {
+        type: "list",
+        name: "type",
+        message: "component type",
+        choices: [
+          { name: "client component", value: "cc" },
+          { name: "server component", value: "sc" },
+          { name: "page", value: "page" },
+          { name: "page component", value: "pc" },
+        ],
+      },
+      {
+        type: "input",
+        name: "path",
+        message: "Where is the path to create components? (e.g. components)",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the component name? (e.g. button)",
+      },
+    ],
+    actions: (data) => {
+      const actions = [];
+      if (typeof data !== "undefined") {
+        if (data.type === "sc") {
+          actions.push(
+            {
+              type: "add",
+              path: "app/{{path}}/{{name}}/index.tsx",
+              templateFile: "generators/component/index.ts.hbs",
+            },
+            {
+              type: "add",
+              path: "app/{{path}}/{{name}}/{{name}}.tsx",
+              templateFile: "generators/component/sc.tsx.hbs",
+            },
+          );
+        } else if (data.type === "cc") {
+          actions.push(
+            {
+              type: "add",
+              path: "components/{{path}}/{{name}}/index.tsx",
+              templateFile: "generators/component/index.ts.hbs",
+            },
+            {
+              type: "add",
+              path: "components/{{path}}/{{name}}/{{name}}.tsx",
+              templateFile: "generators/component/cc.tsx.hbs",
+            },
+          );
+        } else if (data.type === "page") {
+          actions.push({
+            type: "add",
+            path: "app/{{path}}/page.tsx",
+            templateFile: "generators/component/page.tsx.hbs",
+          });
+        } else if (data.type === "pc") {
+          actions.push(
+            {
+              type: "add",
+              path: "app/{{path}}/_components/{{name}}/index.tsx",
+              templateFile: "generators/component/index.ts.hbs",
+            },
+            {
+              type: "add",
+              path: "app/{{path}}/_components/{{name}}/{{name}}.tsx",
+              templateFile: "generators/component/cc.tsx.hbs",
+            },
+          );
+        }
+      }
+
+      return actions;
+    },
+  });
+};
